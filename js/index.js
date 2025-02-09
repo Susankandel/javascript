f4e4d6a9b9cee4cad447ebade7af0db668638060
susan kandel 1 <susankandel99@gmail.com>
2025-02-09 17:58:23 +0545

    variables

--- js/index.js
+++ js/index.js
@@ -1,13 +1,45 @@
 console.log('Hello, world!');
 
 
-const person = 'susan';
+//ES5
+// var
+var name = 'susan'; //variable declaration
+name= 'john'; //override 
+console.log(name);
+
+
+
+// ES6
+// let, const
+const person = 'susan'; //declaration variable
+name= 'john'; // no override
 console.log(person);
 
+let name1 = 'susan'; //allows override
+name = 'sadip';
+console.log(name1); 
 
-let person1 = 'zyf';
-console.log(person1);
 
-var person2 = 'dfg';
-console.log(person2);
+
+////
+
+// alert('Hello, world!');
+
+// const user = prompt('What is your name?');
+// console.log(user);
+
+// const newuser = confirm('Are you 18 plus');
+// console.log(newuser);
+
+
+// write a program that ask user name and alert their name
+//with greetings
+
+//1
+const username = prompt('What is your name?');
+alert('Hello, ' + username + '!');
+
+//string literals
+alert(`Hello, ${username}!`);
+
 

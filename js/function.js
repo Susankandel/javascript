63a359a5bbc148424f1fe0dbc880751fa894533c
susan kandel 1 <susankandel99@gmail.com>
2025-02-16 16:42:40 +0545

    function

--- js/function.js
+++ js/function.js
@@ -21,11 +21,11 @@
 //Function decleration / Initalization
 //input are called parameters, arguments
 
-function sum(num1 ,num2){
-    const result= num1+num2;
-    return result;
+// function sum(num1 ,num2){
+//     const result= num1+num2;
+//     return result;
 
-}
+// }
 
 
 //input data
@@ -46,14 +46,14 @@ function sum(num1 ,num2){
 
 //write a function to calculate the area of circle pr2
 
-function area(radius){
-    return 3.14 * +radius * +radius;
-}
+// function area(radius){
+//     return 3.14 * +radius * +radius;
+// }
 
 //function call
 
-console.log(area("4"));
-console.log(area(4))
+// console.log(area("4"));
+// console.log(area(4))
 
 
 
@@ -61,7 +61,7 @@ console.log(area(4))
 1.Arrow Function(ES6)
 2.Default Function
 3. Parameterization Function
-4. closure 
+4. closure  100% interview asked question
 5. IIFEs
 6. Explicit Function
 7. Implicit Function
@@ -69,11 +69,73 @@ console.log(area(4))
 9. Inliner Function
 10. Callback Function
 
-/*
-
-
-
-
-
+*/
 
 //ES6
+//funcrion arrey
+
+
+//function sum(num1,num2){
+//const result = num1,num2
+//return result
+//}
+
+//Arrow function
+//Function Define
+// const sum = (num1, num2) => {
+//     const result = num1 + num2;
+//     return result;
+// };
+
+//function call
+// console.log(sum(2,2));
+
+
+//Default Function
+
+// const pagination = (page) => {
+//     return `page number ${page}`;
+// };
+
+// console.log(pagination(2));
+// console.log(pagination(1));
+
+
+// //write a function that say welcome person
+// //if user name is provided else it says welcome user
+
+// const userName= (person="userName") => {
+//     return (`welcome ${userName}`)
+// }
+
+// console.log(userName(susan));
+// console.log(userName())
+
+// // Parameterization Function
+
+// const user = ({user1, user2, user3}) => {
+//     return `${user1},${user2}, ${user3}`;
+// };
+
+// console.log(users{{user1:"r", user2:"a", user3:"m"}});
+
+//Write a parameterization Function to calculate the volume of cuboid
+//volume= l*b*h
+
+const volume =({length,breath, height})=>{
+return length* breath*height
+};
+
+console.log(volume({length:2, breath:4, height:4}))
+
+//IIFEs (Immediately Invoked Function Expression)
+
+// (()=> {})();
+
+((a="test")=>{
+    console.log ("IIFES", a)
+})(test);
+
+
+
+
